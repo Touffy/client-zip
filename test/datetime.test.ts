d90a06ab6ca90c5b7ec340a79694f596b9b3da99
David Junger <tffy@free.fr>
2020-05-15 12:57:16 +0200

    removed timezone from date string in test so it should work anywhere

--- test/datetime.test.ts
+++ test/datetime.test.ts
@@ -1,9 +1,8 @@
 import { formatDOSDateTime } from "../src/datetime"
 
 describe("The DOS datetime module", () => {
-  // warning: this test works only in the Europe/Paris or equivalent timezone
   it("should encode dates to local 32-bit DOS format", () => {
-    const date = new Date("2020-02-15T11:24:18+01:00")
+    const date = new Date("2020-02-15T11:24:18")
     const actual = formatDOSDateTime(date)
     const expected = 0x095b4f50
     expect(actual).toEqual(expected)
