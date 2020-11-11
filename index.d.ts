b40c60bc2227049a2a2a0656032c8ddad109c83c
Ran Benita <ran@unusedvar.com>
2020-11-11 15:56:06 +0200

    add a couple of missing type annotations in index.d.ts
    Without these, a project using strict TypeScript settings emits an
    errors like "Member 'name' implicitly has an 'any' type.".
    
    These annotations match the types in src/index.ts.

--- index.d.ts
+++ index.d.ts
@@ -3,10 +3,10 @@ type StreamLike = Blob | ReadableStream<Uint8Array> | AsyncIterable<BufferLike>
 
 /** The file name and modification date will be read from the input;
  * extra arguments can be given to override the input's metadata. */
-type InputWithMeta = File | Response | { input: File | Response, name?, lastModified?}
+type InputWithMeta = File | Response | { input: File | Response, name?: any, lastModified?: any}
 
 /** The file name must be provided with those types of input, and modification date can't be guessed. */
-type InputWithoutMeta = { input: BufferLike | StreamLike, name, lastModified? }
+type InputWithoutMeta = { input: BufferLike | StreamLike, name: any, lastModified?: any }
 
 type ForAwaitable<T> = AsyncIterable<T> | Iterable<T>
 
