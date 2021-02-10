319bbece66facc8813a7fda186b742b2faf9ade7
David Junger <tffy@free.fr>
2021-02-10 09:14:46 +0100

    prettify some numbers

--- src/zip.ts
+++ src/zip.ts
@@ -3,10 +3,10 @@ import { crc32 } from "./crc32.ts"
 import { formatDOSDateTime } from "./datetime.ts"
 import { ZipFileDescription } from "./input.ts"
 
-const fileHeaderSignature = 0x504b0304, fileHeaderLength = 30
-const descriptorSignature = 0x504b0708, descriptorLength = 16
-const centralHeaderSignature = 0x504b0102, centralHeaderLength = 46
-const endSignature = 0x504b0506, endLength = 22
+const fileHeaderSignature = 0x504b_0304, fileHeaderLength = 30
+const descriptorSignature = 0x504b_0708, descriptorLength = 16
+const centralHeaderSignature = 0x504b_0102, centralHeaderLength = 46
+const endSignature = 0x504b_0506, endLength = 22
 
 export async function* loadFiles(files: AsyncIterable<ZipFileDescription>) {
   const centralRecord: Uint8Array[] = []
@@ -48,8 +48,7 @@ export async function* loadFiles(files: AsyncIterable<ZipFileDescription>) {
 export function fileHeader(file: ZipFileDescription) {
   const header = makeBuffer(fileHeaderLength)
   header.setUint32(0, fileHeaderSignature)
-  header.setUint16(4, 0x1400) // version 2.0
-  header.setUint16(6, 0x0800) // flags, bit 3 on = size and CRCs will be zero
+  header.setUint32(4, 0x14_00_0800) // ZIP version 2.0 | flags, bit 3 on = size and CRCs will be zero
   // leave compression = zero (2 bytes) until we implement compression
   formatDOSDateTime(file.modDate, header, 10)
   // leave CRC = zero (4 bytes) because we'll write it later, in the central repo
@@ -91,8 +90,7 @@ export function dataDescriptor(file: ZipFileDescription) {
 export function centralHeader(file: ZipFileDescription, offset: number) {
   const header = makeBuffer(centralHeaderLength)
   header.setUint32(0, centralHeaderSignature)
-  header.setUint16(4, 0x1503) // UNIX version 2.1
-  header.setUint16(6, 0x1400) // version 2.0
+  header.setUint32(4, 0x1503_14_00) // UNIX app version 2.1 | ZIP version 2.0
   header.setUint16(8, 0x0800) // flags, bit 3 on
   // leave compression = zero (2 bytes) until we implement compression
   formatDOSDateTime(file.modDate, header, 12)
