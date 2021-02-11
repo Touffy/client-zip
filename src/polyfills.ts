8e928e69f86d77aefc944ee616a79b9db52ffc07
David Junger <tffy@free.fr>
2021-02-11 19:30:44 +0100

    polyfill setBigUint64 for Safari

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,6 +1,14 @@
-declare interface Blob {
-  stream(): ReadableStream<Uint8Array>
-}
 if (!("stream" in Blob.prototype)) Object.defineProperty(Blob.prototype, "stream", {
-  value() { return new Response(this).body }
+  value(this: Blob) { return new Response(this).body }
 })
+
+if (!("setBigUint64" in DataView.prototype)) Object.defineProperty(DataView.prototype, "setBigUint64", {
+  value(this: DataView, byteOffset: number, value: bigint, littleEndian?: boolean) {
+    const lowWord = Number(value & 0xffffffffn)
+    const highWord = Number(value >> 32n)
+    this.setUint32(byteOffset + (littleEndian ? 0 : 4), lowWord, littleEndian)
+    this.setUint32(byteOffset + (littleEndian ? 4 : 0), highWord, littleEndian)
+  }
+})
+
+export {}
