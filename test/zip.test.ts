70dcf7235e7903dceba667e138e5d972ea63c1a1
Atte Sairanen <atte.sairanen@gmail.com>
2022-06-24 02:01:01 +0300

    More tests for language encoding flag.

--- test/zip.test.ts
+++ test/zip.test.ts
@@ -1,9 +1,13 @@
-import { assertEquals, assertStrictEquals } from "https://deno.land/std/testing/asserts.ts"
+import { assertEquals, assertStrictEquals } from "https://deno.land/std@0.132.0/testing/asserts.ts"
+import { Buffer } from "https://deno.land/std@0.132.0/io/buffer.ts"
 import { fileHeader, fileData, dataDescriptor, centralHeader, contentLength } from "../src/zip.ts"
 import type { ZipFileDescription } from "../src/input.ts"
 import type { Metadata } from "../src/metadata.ts";
 
 const BufferFromHex = (hex: string) => new Uint8Array(Array.from(hex.matchAll(/.{2}/g), ([s]) => parseInt(s, 16)))
+
+const BufferToHex = (buffer: Uint8Array) => 
+  Array.from(buffer, (b) => b.toString(16).padStart(2, "0")).join("")
 
 const zipSpec = Deno.readFileSync("./test/APPNOTE.TXT")
 const specName = new TextEncoder().encode("APPNOTE.TXT")
@@ -18,9 +22,23 @@ Deno.test("the ZIP fileHeader function makes file headers", () => {
   assertEquals(actual, expected)
 })
 
+Deno.test("the ZIP fileHeader function makes file headers with EFS off", () => {
+  const file = {...baseFile}
+  const actual = fileHeader(file, false)
+  const expected = BufferFromHex("504b030414000800000000109a4e0000000000000000000000000b000000")
+  assertEquals(actual, expected)
+})
+Deno.test("the ZIP fileHeader function makes file headers with non-UTF-8 encoded names", () => {
+  const file = {...baseFile}
+  file.isEncodedNameUtf8 = false // encodedName is still utf-8 encoded but let's pretend it is not
+  const actual = fileHeader(file)
+  const expected = BufferFromHex("504b030414000800000000109a4e0000000000000000000000000b000000")
+  assertEquals(actual, expected)
+})
+
 Deno.test("the ZIP fileData function yields all the file's data", async () => {
   const file = {...baseFile}
-  const actual = new Deno.Buffer()
+  const actual = new Buffer()
   for await (const chunk of fileData(file)) actual.writeSync(chunk)
   assertEquals(actual.bytes({copy: false}), zipSpec)
 })
@@ -49,14 +67,31 @@ Deno.test("the ZIP centralHeader function makes central record file headers", ()
   assertEquals(actual, expected)
 })
 
-Deno.test("the contentLength function does not throw on zero-length files", () => {
-  const actual = contentLength([{uncompressedSize: 0, encodedName: specName}])
-  const expected = 136
+Deno.test("the ZIP centralHeader function makes central record file headers with EFS off", () => {
+  const file = {...baseFile, uncompressedSize: 0x10203040, crc: 0x12345678}
+  const offset = 0x01020304
+  const actual = centralHeader(file, offset, false)
+  const expected = BufferFromHex("504b0102150314000800000000109a4e7856341240302010403020100b0000000000000000000000b48104030201")
+  assertEquals(actual, expected)
+})
+
+Deno.test("the ZIP centralHeader function makes central record file heahers with non-UTF-8 encoded names", () => {
+  const file = {...baseFile, uncompressedSize: 0x10203040, crc: 0x12345678}
+  file.isEncodedNameUtf8 = false // encodedName is still utf-8 encoded but let's pretend it is not
+  const offset = 0x01020304
+  const actual = centralHeader(file, offset)
+  const expected = BufferFromHex("504b0102150314000800000000109a4e7856341240302010403020100b0000000000000000000000b48104030201")
   assertEquals(actual, expected)
 })
 
 Deno.test("the contentLength function accurately predicts the length of an archive", () => {
   const actual = contentLength([{uncompressedSize: zipSpec.byteLength, encodedName: specName}])
   const expected = 171462
+  assertEquals(actual, expected)
+})
+
+Deno.test("the contentLength function does not throw on zero-length files", () => {
+  const actual = contentLength([{uncompressedSize: 0, encodedName: specName}])
+  const expected = 136
   assertEquals(actual, expected)
 })
